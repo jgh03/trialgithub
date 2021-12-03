0d150172c4e7d604cbca0a8d2da17571ff122ca6
jgh03 <92313552+jgh03@users.noreply.github.com>
2021-12-03 09:31:38 +0700

    Update Program.cs

--- trialgit/Program.cs
+++ trialgit/Program.cs
@@ -5,8 +5,9 @@ namespace trialgit
     class Program
     {
         static void Main(string[] args)
+ 
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
         }
     }
 }
